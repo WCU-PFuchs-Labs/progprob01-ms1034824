01f6060bdad445b7d620123e5c7139792a117568
ms1034824 <ms1034824@wcupa.edu>
2025-12-01 21:00:20 -0500

    Attempt 2

--- homework20_1.java
+++ homework20_1.java
@@ -15,52 +15,42 @@ output: head-->1-->2-->3-->null
 
 Please complete the following program to fullfil the function.
 */
+/*
+Given a sorted linked list, delete all nodes that have duplicate
+numbers, leaving only distinct numbers from the original list.In this 
+program, you need to 
+(1) read 5 numbers and set them in a linkedlist in order
+(2) delete duplicate
+
+Example 1:
+input: 1,2,3,3,5
+output: head-->1-->2-->3-->5-->null
+
+Example 2:
+input: 1,1,1,2,3
+output: head-->1-->2-->3-->null
+
+Please complete the following program to fullfil the function.
+*/
 public class homework20_1
 {
    public static void main(String[] args)
    {
-    Scanner sc = new Scanner(System.in);
-      LinkedList list = new LinkedList();
+      //add your code here
 
-      for (int i = 0; i < 5; i++) {
-         int v = sc.nextInt();
-         insertSorted(list, v);
-      }
-      deleteDuplicates(list);
-
-      System.out.println(list);
    }
-    public static void insertSorted(LinkedList llist, int v) {
-      ListNode newNode = new ListNode(v);
-
-      if (llist.head == null || v < llist.head.value) {
-         newNode.next = llist.head;
-         llist.head = newNode;
-         return;
-      }
-      ListNode curr = llist.head;
-      while (curr.next != null && curr.next.value <= v) {
-         curr = curr.next;
-      }
-
-      newNode.next = curr.next;
-      curr.next = newNode;
-   }
-
    public static void deleteDuplicates(LinkedList llist)
    {
-      if (llist.head == null) return;
+        if (llist.head == null) return;
 
-      ListNode curr = llist.head;
+        ListNode curr = llist.head;
 
-      while (curr != null && curr.next != null) {
-         if (curr.value == curr.next.value) {
-            // skip the duplicate
-            curr.next = curr.next.next;
-         } else {
-            curr = curr.next;
-         }
-      }
+        while (curr != null && curr.next != null) {
+            if (curr.value == curr.next.value) {
+                curr.next = curr.next.next; // skip duplicate
+            } else {
+                curr = curr.next;
+            }
    }
 
 }
@@ -86,4 +76,5 @@ class LinkedList
       }
       return "head-->"+nodeData+"null";
    }
+}
 }
