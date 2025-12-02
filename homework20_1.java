2f0e2bfeeb93fb0a486c42c4ef8b5aea01cf5bfe
ms1034824 <ms1034824@wcupa.edu>
2025-12-01 21:03:49 -0500

    Update homework20_1.java

--- homework20_1.java
+++ homework20_1.java
@@ -32,49 +32,46 @@ output: head-->1-->2-->3-->null
 
 Please complete the following program to fullfil the function.
 */
+import java.util.Scanner;
+
 public class homework20_1
 {
-   public static void main(String[] args)
-   {
-      //add your code here
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        LinkedList list = new LinkedList();
 
-   }
-   public static void deleteDuplicates(LinkedList llist)
-   {
+        for (int i = 0; i < 5; i++) {
+            int v = sc.nextInt();
+            ListNode newNode = new ListNode(v);
+
+            if (list.head == null) {
+                list.head = newNode;
+            } else {
+                ListNode curr = list.head;
+                while (curr.next != null) {
+                    curr = curr.next;
+                }
+                curr.next = newNode;
+            }
+        }
+
+        deleteDuplicates(list);
+        System.out.println(list);
+    }
+
+    public static void deleteDuplicates(LinkedList llist)
+    {
         if (llist.head == null) return;
 
         ListNode curr = llist.head;
 
         while (curr != null && curr.next != null) {
             if (curr.value == curr.next.value) {
-                curr.next = curr.next.next; // skip duplicate
+                curr.next = curr.next.next; 
             } else {
                 curr = curr.next;
             }
-   }
-
-}
-
-class ListNode
-{
-   int value;
-   ListNode next;
-   ListNode(int v){value = v;}
-}
-
-class LinkedList
-{
-   ListNode head;
-   public String toString()
-   {
-      String nodeData = "";
-      ListNode ref = head;
-      while(ref != null)
-      {
-         nodeData += ref.value + "-->";
-         ref = ref.next;
-      }
-      return "head-->"+nodeData+"null";
-   }
-}
+        }
+    }
 }
